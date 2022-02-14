10fc5ab1c8f53fa0148fc305f19169ceb34dd855
Lukáš Peťko <luke.petko@gmail.com>
2022-02-14 14:09:28 +0100

    map + ios build

---
+++ screens/MapScreen.tsx
@@ -0,0 +1,51 @@
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { styles } from "../styles/global";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+import * as Location from "expo-location";
+
+const MapScreen = () => {
+    const [mapRegion, setmapRegion] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            const { latitude, longitude } = location.coords;
+            setmapRegion({
+                latitude,
+                longitude,
+                latitudeDelta: 0.002,
+                longitudeDelta: 0.002,
+            });
+            setIsLoading(false);
+        })();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            {!isLoading && (
+                <MapView
+                    // provider={PROVIDER_GOOGLE}
+                    style={{ alignSelf: "stretch", height: "100%" }}
+                    region={mapRegion}
+                    showsUserLocation={true}
+                />
+            )}
+        </View>
+    );
+};
+
+export default MapScreen;
