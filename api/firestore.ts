87af095eecd2d94b9185384135f53dd659dc11cc
Lukáš Peťko <luke.petko@gmail.com>
2022-02-20 09:37:51 +0100

    firestore javadoc

--- api/firestore.ts
+++ api/firestore.ts
@@ -9,6 +9,12 @@ import { db } from "../Firebase";
 import { BasicUserInfo } from "../utils/types/basicUserInfo";
 import { User } from "../utils/types/user";
 
+/**
+ * Creates a new user doc in the users collection after a first registration
+ * @param id ID of the user from firebase auth
+ * @param userInfo Other user info
+ * @returns a `boolean` if user was created successfully
+ */
 export const createUserAccount = async (
     id: string,
     userInfo: BasicUserInfo,
@@ -38,6 +44,11 @@ export const createUserAccount = async (
         .catch(() => false);
 };
 
+/**
+ * Gets a user doc from the users collection based on the user's ID
+ * @param id user ID from firebase auth
+ * @returns a `User` object
+ */
 export const getUserById = async (id: string): Promise<DocumentData | null> => {
     const userDoc: DocumentReference<DocumentData> = doc(db, "users", id);
     const userSnap: DocumentData = await getDoc(userDoc);
