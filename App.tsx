fd5a0536ab45d4caa4bd0c8987c74f9ea16294dd
Lukáš Peťko <luke.petko@gmail.com>
2022-04-26 17:55:46 +0200

    notifications

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Provider, useDispatch } from "react-redux";
-import { getData } from "./async-storage";
+import { Provider } from "react-redux";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
@@ -10,10 +9,50 @@ import Navigation from "./navigation";
 import store from "./redux/store";
 
 import "./utils/location";
+import {
+    registerForPushNotificationsAsync,
+    sendPushNotification,
+} from "./utils/notifications";
+import * as Notifications from "expo-notifications";
+import { Subscription } from "expo-modules-core";
+import { Button } from "react-native";
 
 const App = (): JSX.Element | null => {
+    const [expoPushToken, setExpoPushToken] = useState<string>("");
+    const [notification, setNotification] = useState(false);
+    const notificationListener: MutableRefObject<Subscription> = useRef();
+    const responseListener: MutableRefObject<Subscription> = useRef();
+
     const isLoadingComplete = useCachedResources();
     const colorScheme = useColorScheme();
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then((token) =>
+            setExpoPushToken(token),
+        );
+
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener((notification) => {
+                // setNotification(notification as Boolean);
+                console.log("notification", notification);
+            });
+
+        responseListener.current =
+            Notifications.addNotificationResponseReceivedListener(
+                (response) => {
+                    console.log(response);
+                },
+            );
+
+        return () => {
+            Notifications.removeNotificationSubscription(
+                notificationListener.current,
+            );
+            Notifications.removeNotificationSubscription(
+                responseListener.current,
+            );
+        };
+    }, []);
 
     if (!isLoadingComplete) {
         return null;
